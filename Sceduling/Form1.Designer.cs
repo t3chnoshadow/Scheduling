4d9dd57843dc8f5b213828fff6b0bc2afd2c86bd
ZLombaard <48584857+ZLombaard@users.noreply.github.com>
2019-03-16 14:12:35 +0200

    Zander

--- Sceduling/Form1.Designer.cs
+++ Sceduling/Form1.Designer.cs
@@ -47,20 +47,20 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(457, 236);
+            this.button2.Location = new System.Drawing.Point(433, 236);
             this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.Size = new System.Drawing.Size(175, 28);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(457, 332);
+            this.button3.Location = new System.Drawing.Point(433, 332);
             this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 28);
+            this.button3.Size = new System.Drawing.Size(175, 28);
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
