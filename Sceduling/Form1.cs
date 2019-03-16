fd746983f6899e5e4127b35acfe16910b040b7ca
ZLombaard <48584857+ZLombaard@users.noreply.github.com>
2019-03-16 13:58:54 +0200

    toets

--- Sceduling/Form1.cs
+++ Sceduling/Form1.cs
@@ -16,5 +16,10 @@ namespace Sceduling
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Jipeee");
+        }
     }
 }
